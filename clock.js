bb0375ca57eb7c4f273067f4095212220b74edbf
gtossou <gil.tososu@gmail.com>
2019-12-19 22:32:54 +0100

    day and month conversion functions

---
+++ clock.js
@@ -0,0 +1,15 @@
+const dayToString= function (weekDay){
+    return days[weekDay-1]
+}
+
+const monthToString= function (currentMonth){
+    return months[currentMonth]
+}
+
+const months=["January","February","March","April","May","June","July","August","September","October","November","December"]
+const days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+const today=new Date();
+
+
+
