5063fc7829a23772c744d9e25aa855f5c869e9da
gtossou <gil.tososu@gmail.com>
2019-12-30 17:08:31 +0100

    classes to id

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 ## Vanilla Javascript Clock
 
-*** Simple clock in Vanilla Javascript and HTML + CSS *** 
+**_Simple clock in Vanilla Javascript and HTML + CSS_**
 
 ![Clock app](./img/example.jpg)
 
 ### Summary
 
 My first app done in my roadmap to become a fullstack web developper.
-Done in few hours, it made me apply the bascis of callbacks functions and DOM manipulation.
-
+Done in few hours, it made me apply the basics of callback functions and DOM manipulation.
--- js/clock.js
+++ js/clock.js
@@ -26,18 +26,18 @@ const tickSecond = function(){
     let today = new Date();
     // check user format 
     if (toggleValue == "locale"){
-        document.querySelector(".seconds").innerHTML = checkTrailingzero(today.getSeconds());
+        document.querySelector("#seconds").innerHTML = checkTrailingzero(today.getSeconds());
     }
     else{
         pmAm = today.toLocaleString('en-US').split(" ",)[2]
-        document.querySelector(".seconds").innerHTML = checkTrailingzero(today.getSeconds())+" "+pmAm;
+        document.querySelector("#seconds").innerHTML = checkTrailingzero(today.getSeconds())+" "+pmAm;
     }
 }
 
 // tick minute
 const tickMinute = function(){
     let today = new Date();
-    document.querySelector(".minutes").innerHTML = checkTrailingzero(today.getMinutes());
+    document.querySelector("#minutes").innerHTML = checkTrailingzero(today.getMinutes());
 }
 
 // tick hour
@@ -45,25 +45,25 @@ const tickHour = function(toggleValue){
     let today = new Date();
     // check user format
     if (toggleValue == "locale"){
-    document.querySelector(".hours").innerHTML = checkTrailingzero(today.getHours());
+    document.querySelector("#hours").innerHTML = checkTrailingzero(today.getHours());
     }
     else{
         today = today.toLocaleString('en-US')
         hourUS = (today.split(':')[0].split(",",)[1])
-        document.querySelector(".hours").innerHTML = checkTrailingzero(hourUS);
+        document.querySelector("#hours").innerHTML = checkTrailingzero(hourUS);
     }
 }
 
 // tick day
 const tickDay = function(){
     let today = new Date();
-    document.querySelector(".day").innerHTML = dayToString(today.getDay());
+    document.querySelector("#day").innerHTML = dayToString(today.getDay());
 }
 
 // tick month
 const tickMonth = function(){
     let today = new Date();
-    document.querySelector(".month").innerHTML = dayToString(today.getMonth());
+    document.querySelector("#month").innerHTML = dayToString(today.getMonth());
 }
 
 const toggleFunction = function(toggleValue){
@@ -90,10 +90,10 @@ const timeDiv = document.createElement("div");
 timeDiv.classList.add("timeDiv");
 
 let timeHtml = `<p class="timeP">
-                <span class="hours">${checkTrailingzero(today.getHours())}</span>
+                <span id="hours "class="hours">${checkTrailingzero(today.getHours())}</span>
                 <span>:</span>
-                <span class="minutes">${checkTrailingzero(today.getMinutes())}</span>
-                <span class="seconds">${checkTrailingzero(today.getSeconds())}</span>
+                <span id="minutes" class="minutes">${checkTrailingzero(today.getMinutes())}</span>
+                <span id="seconds" class="seconds">${checkTrailingzero(today.getSeconds())}</span>
                 </p>`;
 timeDiv.innerHTML = timeHtml;
 bodyDiv.insertAdjacentElement("beforeend",timeDiv)
@@ -101,7 +101,7 @@ bodyDiv.insertAdjacentElement("beforeend",timeDiv)
 // DATE DIV
 const dateDiv = document.createElement("div");
 dateDiv.classList.add("dateDiv");
-let dateHtml = `<p class="dateP"><span class="day">${dayToString(today.getDay())}</span> - <span class="month">${monthToString(today.getMonth())}</span> <span class="date">${today.getDate()}</span></p>`;
+let dateHtml = `<p class="dateP"><span id="day" class="day">${dayToString(today.getDay())}</span> - <span id="month" class="month">${monthToString(today.getMonth())}</span> <span class="date">${today.getDate()}</span></p>`;
 dateDiv.innerHTML=dateHtml;
 
 bodyDiv.insertAdjacentElement("beforeend",dateDiv)
@@ -111,7 +111,7 @@ setInterval(function(){
     tickMinute();
     tickHour(toggleValue);
 }, 1000); 
-butto = document.querySelector(".togButton");
+butto = document.querySelector("#togButton");
 console.log(butto);
 butto.addEventListener('click', function(){
     toggleValue = toggleFunction(toggleValue)
