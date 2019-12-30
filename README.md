1c9970636e0899b0f58a48dce1a1e20ece0d9a49
gtossou <gil.tososu@gmail.com>
2019-12-30 16:55:00 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+## Vanilla Javascript Clock
+
+*** Simple clock in Vanilla Javascript and HTML + CSS *** 
+
+![Clock app](./img/example.jpg)
+
+### Summary
+
+My first app done in my roadmap to become a fullstack web developper.
+Done in few hours, it made me apply the bascis of callbacks functions and DOM manipulation.
+
--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,6 @@
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+const days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
 // day number to string
 const dayToString = function (weekDay){
     return days[weekDay-1]
@@ -10,7 +13,7 @@ const monthToString = function (currentMonth){
 
 // add trailing zero for time
 const checkTrailingzero = function(number){
-    if (parseInt(number).toString().length==2){
+    if (parseInt(number).toString().length == 2){
         return parseInt(number);
     }
     else{
@@ -19,73 +22,71 @@ const checkTrailingzero = function(number){
 }
 
 // tick second
-const tickSecond=function(){
-    let today=new Date();
+const tickSecond = function(){
+    let today = new Date();
     // check user format 
-    if (toggleValue=="locale"){
-        document.querySelector(".seconds").innerHTML=checkTrailingzero(today.getSeconds());
+    if (toggleValue == "locale"){
+        document.querySelector(".seconds").innerHTML = checkTrailingzero(today.getSeconds());
     }
     else{
-        pmAm=today.toLocaleString('en-US').split(" ",)[2]
-        document.querySelector(".seconds").innerHTML=checkTrailingzero(today.getSeconds())+" "+pmAm;
+        pmAm = today.toLocaleString('en-US').split(" ",)[2]
+        document.querySelector(".seconds").innerHTML = checkTrailingzero(today.getSeconds())+" "+pmAm;
     }
 }
 
 // tick minute
-const tickMinute=function(){
-    let today=new Date();
-    document.querySelector(".minutes").innerHTML=checkTrailingzero(today.getMinutes());
+const tickMinute = function(){
+    let today = new Date();
+    document.querySelector(".minutes").innerHTML = checkTrailingzero(today.getMinutes());
 }
 
 // tick hour
-const tickHour=function(toggleValue){
-    let today=new Date();
+const tickHour = function(toggleValue){
+    let today = new Date();
     // check user format
-    if (toggleValue=="locale"){
-    document.querySelector(".hours").innerHTML=checkTrailingzero(today.getHours());
+    if (toggleValue == "locale"){
+    document.querySelector(".hours").innerHTML = checkTrailingzero(today.getHours());
     }
     else{
-        today=today.toLocaleString('en-US')
-        hourUS=(today.split(':')[0].split(",",)[1])
-        document.querySelector(".hours").innerHTML=checkTrailingzero(hourUS);
+        today = today.toLocaleString('en-US')
+        hourUS = (today.split(':')[0].split(",",)[1])
+        document.querySelector(".hours").innerHTML = checkTrailingzero(hourUS);
     }
 }
 
 // tick day
-const tickDay=function(){
-    let today=new Date();
-    document.querySelector(".day").innerHTML=dayToString(today.getDay());
+const tickDay = function(){
+    let today = new Date();
+    document.querySelector(".day").innerHTML = dayToString(today.getDay());
 }
 
 // tick month
-const tickMonth=function(){
-    let today=new Date();
-    document.querySelector(".month").innerHTML=dayToString(today.getMonth());
+const tickMonth = function(){
+    let today = new Date();
+    document.querySelector(".month").innerHTML = dayToString(today.getMonth());
 }
 
 const toggleFunction = function(toggleValue){
-    if (toggleValue=="locale"){
-        toggleValue="US"
+    if (toggleValue == "locale"){
+        toggleValue = "US"
     }
     else{
-        toggleValue="locale"
+        toggleValue = "locale"
     }
     return toggleValue;
 }
-const months=["January","February","March","April","May","June","July","August","September","October","November","December"]
-const days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 
 // vars
-let toggleValue="locale";
-const today=new Date();
+let toggleValue = "locale";
+const today = new Date();
 
 // BODY DIV
-const bodyDiv=document.createElement("div");
+const bodyDiv = document.createElement("div");
 bodyDiv.classList.add("bodyDiv");
 document.body.appendChild(bodyDiv);
 
 // TIME DIV
-const timeDiv=document.createElement("div");
+const timeDiv = document.createElement("div");
 timeDiv.classList.add("timeDiv");
 
 let timeHtml = `<p class="timeP">
@@ -94,14 +95,15 @@ let timeHtml = `<p class="timeP">
                 <span class="minutes">${checkTrailingzero(today.getMinutes())}</span>
                 <span class="seconds">${checkTrailingzero(today.getSeconds())}</span>
                 </p>`;
-timeDiv.innerHTML=timeHtml;
+timeDiv.innerHTML = timeHtml;
 bodyDiv.insertAdjacentElement("beforeend",timeDiv)
 
 // DATE DIV
-const dateDiv=document.createElement("div");
+const dateDiv = document.createElement("div");
 dateDiv.classList.add("dateDiv");
 let dateHtml = `<p class="dateP"><span class="day">${dayToString(today.getDay())}</span> - <span class="month">${monthToString(today.getMonth())}</span> <span class="date">${today.getDate()}</span></p>`;
 dateDiv.innerHTML=dateHtml;
+
 bodyDiv.insertAdjacentElement("beforeend",dateDiv)
 
 setInterval(function(){
@@ -109,9 +111,9 @@ setInterval(function(){
     tickMinute();
     tickHour(toggleValue);
 }, 1000); 
-butto=document.querySelector(".togButton");
+butto = document.querySelector(".togButton");
 console.log(butto);
 butto.addEventListener('click', function(){
-    toggleValue=toggleFunction(toggleValue)
+    toggleValue = toggleFunction(toggleValue)
 });
 
